b3f2423cad25ac363e6f845181d8aae57fbc54cd
Tim Aspesberro <taspes@gmail.com>
2023-03-09 23:21:34 -0500

    worked on html/bootstrap as well as script file

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,29 @@
 var searchBtn = $('#searchBtn');
+var cityInput = $('cityInput').value();
+var weatherDis = $('weatherDisplay');
+var cardOne = $('card1');
+var cardTwo = $('card2');
+var cardThree = $('card3');
+var cardFour = $('card4');
+var cardFive = $('card5');
 
+var apiKey = '7d74e270df0bbca669a7d6d2d8197ca6';
+
+var latitude = '';
+var longitude = '';
+
+
+const getApi = (longitude, latitude) => {
+    var requestUrl = `api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+      });
+}
+
+
+searchBtn.addEventListener('click', getApi(latitude,longitude))
