fdb76e13933366b4f66c5a32c60961c3d5914817
Tim Aspesberro <taspes@gmail.com>
2023-03-15 00:47:52 -0400

    added more functionality

--- assets/script.js
+++ assets/script.js
@@ -12,42 +12,75 @@ let latitude = 0;
 let longitude = 0;
 var apiKey = 'bc12083e70d2d22298c2df1cec7101d9';
 
-var city = 'denver';
-var state = 'co';
+var city = 'los angeles';
+var state = 'ca';
+
 
 const setCoordinate = function (lon, lat,) {
   longitude = lon;
   latitude = lat;
 }
 
-const getApi = function(){
-  console.log(latitude, longitude, apiKey)
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
-  .then(response => {
-  return response.json();
-  }).then(data => console.log(data))
-  .catch(err => {
-  console.log(`Error: ${err}`)
-  })
-};
-
 const getCity = function() {
-
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},+1&limit=1&appid=bc12083e70d2d22298c2df1cec7101d9`)
   .then(response => {
   return response.json();
   }).then(data => {
-    console.log(data)
     return setCoordinate(data[0].lon, data[0].lat)
   }).catch(err => {
   console.log(`Error: ${err}`)
   })
 }
+
+const getApi = function(){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+  .then(response => {
+    return response.json();
+  }).then(weatherData => {
+      console.log(weatherData)
+      startForecast(weatherData, city);
+    }
+  ).catch(err => {
+      console.log(`Error: ${err}`)
+  })
+};
+
+const startForecast = function (weatherData, city) {
+  makeDaily(weatherData, city);
+  //makeForecast(weatherData);
+}
+
+const makeDaily = function (weatherData, city) {
+    var curWeather = weatherData.weather[0].description;
+    var curTemp = Math.floor(weatherData.main.temp);
+    var minTemp = Math.floor(weatherData.main.temp_min);
+    var maxTemp = Math.floor(weatherData.main.temp_max);
+    var iconId = weatherData.weather[0].icon;
+  
+    document.getElementById('weatherDisplay').innerHTML = 
+      `<div>
+        <h3>${city.toUpperCase()}</h3>
+        <h5>${curWeather.toUpperCase()}</h5>
+        <div id="icon"><img id="wicon" src="https://openweathermap.org/img/wn/${iconId}@2x.png" alt="Weather icon"></div>
+          <ul class="list-unstyled">
+            <li>Temperature: ${curTemp}°F</li>
+            <li>Max Temp: ${maxTemp}°F</li>
+            <li>Min Temp: ${minTemp}°F</li>
+          </ul>
+      </div>`;
+};
+
+const makeForecast = function () {
+
+};
     
 getCity();
 setTimeout(function(){
   getApi();
-}, 5000)
+}, 1000)
+
+
+
 
 
 //document.getElementById('searchBtn').addEventListener('click', getCity());
